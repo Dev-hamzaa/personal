f4c0546366d776e7026d56e3dc10a6031f9ff334
dev-hamzaa <xzerefff@gmail.com>
2025-01-31 22:03:21 +0500

    login page

--- flutterapp/lib/consts/strings.dart
+++ flutterapp/lib/consts/strings.dart
@@ -2,5 +2,9 @@ class AppStrings {
   static String appName = "Health Sphere",
       signUpNow = "Sign up now and explore",
       confirmPasswors = "confirm Password",
-      fullName = "Full Name";
+      fullName = "Full Name",
+      emailHint = "Enter your email",
+      passwordHint = "Enter your password",
+      forgotPassword = "Forgot Password?",
+      login = "Login";
 }
--- flutterapp/lib/views/login/loginview.dart
+++ flutterapp/lib/views/login/loginview.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapp/components/custom-textfield.dart';
 import 'package:flutterapp/consts/images.dart';
+import 'package:flutterapp/consts/strings.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
@@ -20,10 +23,28 @@ class LoginView extends StatelessWidget {
             Expanded(
                 flex: 2,
                 child: Container(
-                  color: Colors.greenAccent,
-                  // child: Column(
-                  //   children: [],
-                  // ),
+                  // color: Colors.greenAccent,
+                  child: Form(
+                      child: Column(
+                    children: [
+                      CustomTextField(
+                        hint: AppStrings.emailHint,
+                      ),
+                      10.heightBox,
+                      CustomTextField(hint: AppStrings.passwordHint),
+                      20.heightBox,
+                      Align(
+                          alignment: Alignment.centerRight,
+                          child: AppStrings.forgotPassword.text.make()),
+                      20.heightBox,
+                      SizedBox(
+                        width: context.screenWidth - 60,
+                        child: ElevatedButton(
+                            onPressed: () {},
+                            child: AppStrings.login.text.make()),
+                      )
+                    ],
+                  )),
                 ))
           ],
         ),
