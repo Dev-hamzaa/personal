aead96bcb613c9f2b4c397de9f0c29b752994e9e
dev-hamzaa <xzerefff@gmail.com>
2025-03-08 03:18:45 +0500

    donor Side is completed

--- flutterapp/lib/views/patient/donorListView.dart
+++ flutterapp/lib/views/patient/donorListView.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutterapp/consts/endPoints.dart';
-import 'package:velocity_x/velocity_x.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutterapp/components/header.dart';
 
